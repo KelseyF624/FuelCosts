28f35e2397882df39db95d6cc6c6efb3135bf20c
KelseyF624 <kelseyf.624@gmail.com>
2025-02-17 15:07:49 -0500

    Complete Commit

--- src/FuelCosts.java
+++ src/FuelCosts.java
@@ -2,34 +2,30 @@ import java.util.Scanner;
 public class FuelCosts {
     public static void main(String[] args) {
         double gallons;
+        Scanner input = new Scanner(System.in);
         do {
             System.out.println("Please enter the number of gallons of gas in the tank.");
-            Scanner scanner = new Scanner(System.in);
-            gallons = scanner.nextDouble();
-            scanner.close();
-            if (gallons < 0)
-                System.out.println("Invalid gallons");
-        }while (gallons > 0);
+            gallons = input.nextDouble();
+            if (gallons <= 0){
+                System.out.println("Invalid gallons.");}
+        }while (gallons <= 0);
         double fuelEfficiency;
         do {
-            System.out.println("Please enter the fue l efficiency in miles per gallon.");
-            Scanner scanner = new Scanner(System.in);
-            fuelEfficiency = scanner.nextDouble();
-            scanner.close();
-            if (fuelEfficiency < 0)
-                System.out.println("Invalid fuel efficiency");
-        }while (fuelEfficiency > 0);
+            System.out.println("Please enter the fuel efficiency in miles per gallon.");
+            fuelEfficiency = input.nextDouble();
+            if (fuelEfficiency <= 0){
+                System.out.println("Invalid fuel efficiency");}
+        }while (fuelEfficiency <= 0);
         double gasPrice;
         do {
             System.out.println("Please enter the price of gas per gallon.");
-            Scanner scanner = new Scanner(System.in);
-            gasPrice = scanner.nextDouble();
-            scanner.close();
-            if (gasPrice < 0)
-                System.out.println("Invalid gas");
-        }while (gasPrice > 0);
-        double drive100 = gallons / 100 * gasPrice;
-        double distanceWithFuel = gallons / 100 * fuelEfficiency;
+            gasPrice = input.nextDouble();
+            if (gasPrice <= 0){
+                System.out.println("Invalid gas");}
+        }while (gasPrice <= 0);
+        input.close();
+        double drive100 = (100/fuelEfficiency) * gasPrice;
+        double distanceWithFuel = gallons * fuelEfficiency;
         System.out.println("The cost to drive 100 miles is " + drive100);
         System.out.println("The total distance with the fuel is " + distanceWithFuel);
     }
