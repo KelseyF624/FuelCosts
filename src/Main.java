88d1e4c41057423ec344e95ad6336a71b28f69d0
KelseyF624 <kelseyf.624@gmail.com>
2025-02-17 13:34:45 -0500

    Partial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("Please enter the number of gallons of gas in the tank.");
+            Scanner scanner = new Scanner(System.in);
+            int gallons = scanner.nextInt();
+            scanner.close();
+            if (gallons < 0)
+                System.out.println("Invalid gallons");
+        }while (gallons > 0);
+        do {
+            System.out.println("Please enter the fuel efficiency in miles per gallon.");
+            Scanner scanner = new Scanner(System.in);
+            int fuelEfficiency = scanner.nextInt();
+            scanner.close();
+            if (fuelEfficiency < 0)
+                System.out.println("Invalid fuel efficiency");
+        }while (fuelEfficiency > 0);
+        do {
+            System.out.println("Please enter the price of gas per gallon.");
+            Scanner scanner = new Scanner(System.in);
+            int gasPrice = scanner.nextInt();
+            scanner.close();
+            if (gasPrice < 0)
+                System.out.println("Invalid gas");
+        }while (gasPrice > 0);
+        int drive100 = gallons / 100 * gasPrice;
+        int distanceWithFuel = gallons / 100 * fuelEfficiency;
+        System.out.println("The cost to drive 100 miles is " + drive100);
+        System.out.println("The total distance with the fuel is " + distanceWithFuel);
     }
 }
